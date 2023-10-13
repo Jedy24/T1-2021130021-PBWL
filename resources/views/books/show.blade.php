14d050ee0b4778f67c25eedcb02f78b7ae0000c4
Jevon Ivander Juandy <reihimawari27@gmail.com>
2023-10-13 13:09:43 +0700

    Add description on show page

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -5,9 +5,9 @@
 @section('content')
     <article class="blog-post my-4">
         <h1 class="display-5 fw-bold">{{ $book->judul }}</h1>
-        <p class="display-6">{{ $book->halaman }}</p>
-        <p class="display-6">{{ $book->kategori }}</p>
-        <p class="display-6">{{ $book->penerbit }}</p>
+        <p class="display-6">Jumlah halaman : {{ $book->halaman }}</p>
+        <p class="display-6">Kategori : {{ $book->kategori }}</p>
+        <p class="display-6">Penerbit : {{ $book->penerbit }}</p>
         <p class="blog-post-meta">{{ $book->updated_at }}</p>
         <p>{{ $book->body }}</p>
     </article>
